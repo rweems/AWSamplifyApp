8e051c39ec92ad93993a2b226b7b14e48cb696dd
Rickey <weems505@gmail.com>
2020-09-14 16:06:20 -0500

    added auth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+Amplify.configure({
+    "aws_project_region": "us-east-2",
+    "aws_cognito_identity_pool_id": "us-east-2:6b9e4218-9ce9-4baa-9089-8f8d19a2fc36",
+    "aws_cognito_region": "us-east-2",
+    "aws_user_pools_id": "us-east-2_5dWyw4SJ0",
+    "aws_user_pools_web_client_id": "6hoh6kblh1dt0pv2phmotls7d7",
+    "oauth": {}
+});
 
 ReactDOM.render(
   <React.StrictMode>
